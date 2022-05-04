8cfab2e4cc3f49649af21bd55d3051f7b5951d58
JBreece <joshbreece2012@gmail.com>
2022-05-04 10:24:45 -0400

    Shiny basic reactivity

---
+++ testRapp/varRangeFailCode.R
@@ -0,0 +1,20 @@
+library (shiny)
+
+df <- mtcars
+
+ui <- fluidPage(
+	selectInput("var", NULL, choices = colnames(df)),
+	verbatimTextOutput("debug")
+)
+
+
+server <- function(input, output, session) {
+	col_var <- reactive( df[input$var] )
+	col_range <- reactive({ range(col_var(), na.rm = TRUE) })
+	output$debug <- renderPrint({ col_range() })
+
+
+}
+
+
+shinyApp(ui = ui, server = server)
